705ff99177a20b63b6d12a3578eeee19365ec8f2
sinedied <noda@free.fr>
2022-04-06 19:12:38 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+void setup(){
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+
+  Serial.println("Hello Arduino!");
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
